30512496b0631fd80dae7c62403c41fd41889cbc
Sven Heyll <sven.heyll@gmail.com>
2021-01-18 16:56:23 +0100

    Update to haskell-language-server 0.8.0

--- shell.nix
+++ shell.nix
@@ -8,14 +8,13 @@ in
   tools = {
     cabal = "3.2.0.0";
     ormolu = "0.1.4.1";
-    haskell-language-server = "0.6.0";
+    haskell-language-server = "0.8.0";
   };
   buildInputs = with pkgs.haskellPackages;
     [
       tasty-discover
       graphmod
-      weeder
-      hp2pretty
+      hp2pretty      
     ];
 }
 
