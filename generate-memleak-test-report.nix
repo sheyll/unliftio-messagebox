be15055cc5463af604013aa4b22da16c8a3c6827
Sven Heyll <sven.heyll@gmail.com>
2021-01-19 22:17:01 +0100

    Increase memleak test run time

--- generate-memleak-test-report.nix
+++ generate-memleak-test-report.nix
@@ -1,17 +1,18 @@
 let pkgs = import nix/pkgs.nix;
 in
-pkgs.runCommand "memleak-test-report" {
-   memleakTest = import ./memleak-test.nix { withProfiling = true; }; 
-   hp2pretty = pkgs.haskellPackages.hp2pretty;
-  } ''
+pkgs.runCommand "memleak-test-report"
+{
+  memleakTest = import ./memleak-test.nix { withProfiling = true; };
+  hp2pretty = pkgs.haskellPackages.hp2pretty;
+} ''
   mkdir -p $out
   cd $out
 
   echo "Writing into: $out/test.log"
   echo ""
-  echo "This may take more than 13 minutes..."
+  echo "This may take more than 45 minutes..."
   echo ""
-  $memleakTest/bin/unliftio-messagebox-memleak-test 100 10 +RTS -M400m -RTS > test.log
+  $memleakTest/bin/unliftio-messagebox-memleak-test 100 30 +RTS -M400m -RTS > test.log
   
   # produce unliftio-messagebox-memleak-test.svg
   $hp2pretty/bin/hp2pretty unliftio-messagebox-memleak-test.hp
